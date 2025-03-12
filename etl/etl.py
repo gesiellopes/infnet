c1c5d8adfafddc4000f1965f564419a238baf326
gesielrios <gesielrios@gmail.com>
2025-03-11 21:38:22 -0300

    Adicao da chamado do loguro para realizar o log de sucesso do etl.

--- etl/etl.py
+++ etl/etl.py
@@ -1,5 +1,13 @@
 import pandas as pd
+
+
+from loguru import logger
 from pymongo import MongoClient
+from sys import stderr
+
+logger.remove()
+logger.add(stderr, format="{time:HH:mm:ss} | <lvl>{level} </lvl> {level.icon} | <lvl>{message}</lvl>")
+
 
 client = MongoClient('mongodb://127.0.0.1:27017/')
 db = client['datalake']
@@ -10,4 +18,4 @@ df = pd.read_csv('data.csv')
 data = df.to_dict(orient='records')
 collection.insert_many(data)
 
-print('Data inserted successfully')
+logger.success('Data inserted successfully!')
